19f78a4fbae68fc1dd0c954d26e64549cb9eed30
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2022-10-23 19:22:47 +0300

    Style fix

--- tests/tests.rs
+++ tests/tests.rs
@@ -7,8 +7,6 @@ mod anime_list_tests {
         let animes = jutsuparser_rs::search("Две звезды")
             .await
             .expect("Cannot get anime");
-
-
         let first_anime = animes.iter().next().expect("Cannot get anime");
         assert_eq!(first_anime.url, "https://jut.su/onmyouji/")
     }
