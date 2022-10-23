acc6523e6ef353d97412d8f66361df8b8d9164d8
voidptr_t <39582793+Sanceilaks@users.noreply.github.com>
2022-10-23 21:18:54 +0300

    Added episodes parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,6 @@
 use std::collections::HashMap;
 
-use kuchiki::traits::TendrilSink;
-
-pub struct Anime {
-    pub name: String,
-    pub url: String,
-    pub image_url: String
-}
+use kuchiki::{traits::TendrilSink, NodeRef};
 
 pub(crate) fn create_http_client() -> reqwest::Client {
     reqwest::Client::builder()
@@ -15,65 +9,128 @@ pub(crate) fn create_http_client() -> reqwest::Client {
         .expect("Cannot create web client due to")
 }
 
-pub async fn search(anime_name: &str) -> Result<Vec<Anime>, String> {
+pub(crate) async fn get_parsed(url: &str) -> NodeRef {
     let client = create_http_client();
-    let mut params = HashMap::new();
-    params.insert("ajax_load", "yes");
-    params.insert("start_from_page", "1");
-    params.insert("show_search", anime_name);
-    params.insert("anime_of_user", "");
+    let resp = client.get(url).send().await.unwrap();
 
+    let response = resp.error_for_status().expect("Cannot get episode page");
+    let page_content = response.text().await.unwrap();
+    kuchiki::parse_html().one(page_content)
+}
 
-    let request = match client.post("https://jut.su/anime")
-        .form(&params)
-        .header("Referer", "http://jut.su/anime/")
-        .send().await {
-            Ok(o) => o,
-            Err(why) => {
-                return Err(std::format!("Cannot get response with error {:?}", why))
-            }
-    };
+pub struct Anime {
+    pub name: String,
+    pub url: String,
+    pub image_url: String
+}
+
+pub struct Episode {
+    pub url: String
+}
+
+impl Episode {
+    pub fn new(url: &str) -> Episode {
+        Episode { url: (url.to_string()) }
+    }
+
+    pub async fn get_name(&self) -> String {
+        get_parsed(&self.url).await.select_first(".video_plate_title")
+            .unwrap().as_node().select_first("h2").unwrap()
+                .text_contents()
+    }
+}
+
+impl Anime {
+    ///
+    /// Search for anime by his name
+    /// 
+    pub async fn search(anime_name: &str) -> Result<Vec<Anime>, String> {
+        let client = create_http_client();
+        let mut params = HashMap::new();
+        params.insert("ajax_load", "yes");
+        params.insert("start_from_page", "1");
+        params.insert("show_search", anime_name);
+        params.insert("anime_of_user", "");
     
-    let response = match request.error_for_status() {
-        Ok(res) => res,
-        Err(why) => {
-            return Err(std::format!("Cannot get response with error  {:?}", why));
-        }
-    };
-
-    let page_content = response.text().await.expect("Cannot get page content");
-    let document = kuchiki::parse_html().one(page_content);
-
-    let mut anime : Vec<Anime> = Vec::new();
-
-    document.select("div.all_anime_global").unwrap().for_each(|node| {
-        let as_node = node.as_node();
+        let request = match client.post("https://jut.su/anime")
+            .form(&params)
+            .header("Referer", "http://jut.su/anime/")
+            .send().await {
+                Ok(o) => o,
+                Err(why) => {
+                    return Err(std::format!("Cannot get response with error {:?}", why))
+                }
+        };
         
-        let body = as_node.select_first("div.all_anime").unwrap();
-
-        let url: String = as_node.select_first("a").and_then(|n| {
-            let node_n = n.attributes.borrow();
-            Ok(node_n.get("href").unwrap().to_string())
-        }).expect("Cannot get URL");
-
-        let image_style = body.as_node().select_first(".all_anime_image").and_then(|n| {
-            let attrib = n.attributes.borrow();
-            Ok(attrib.get("style").unwrap().to_string())
-        }).expect("Cannot get image style");
-
-        let regex = regex::Regex::new(r"'(https://.*?)'").unwrap();
-        let image = regex.captures_iter(image_style.as_str()).next().and_then(|m| {
-            Some(m.get(1).unwrap().as_str().to_string())
-        }).expect("Cannot get image");
-
-        let name = body.as_node().select_first(".aaname").and_then(|aaname|{
-            Ok(aaname.as_node().text_contents())
-        }).expect("Cannot get anime name");
-
-        anime.push(Anime {
-             name: (name), url: (format!("https://jut.su{}", url)), image_url: (image) 
+        let response = match request.error_for_status() {
+            Ok(res) => res,
+            Err(why) => {
+                return Err(std::format!("Cannot get response with error  {:?}", why));
+            }
+        };
+    
+        let page_content = response.text().await.expect("Cannot get page content");
+        let document = kuchiki::parse_html().one(page_content);
+    
+        let mut anime : Vec<Anime> = Vec::new();
+    
+        document.select("div.all_anime_global").unwrap().for_each(|node| {
+            let as_node = node.as_node();
+            
+            let body = as_node.select_first("div.all_anime").unwrap();
+    
+            let url: String = as_node.select_first("a").and_then(|n| {
+                let node_n = n.attributes.borrow();
+                Ok(node_n.get("href").unwrap().to_string())
+            }).expect("Cannot get URL");
+    
+            let image_style = body.as_node().select_first(".all_anime_image").and_then(|n| {
+                let attrib = n.attributes.borrow();
+                Ok(attrib.get("style").unwrap().to_string())
+            }).expect("Cannot get image style");
+    
+            let regex = regex::Regex::new(r"'(https://.*?)'").unwrap();
+            let image = regex.captures_iter(image_style.as_str()).next().and_then(|m| {
+                Some(m.get(1).unwrap().as_str().to_string())
+            }).expect("Cannot get image");
+    
+            let name = body.as_node().select_first(".aaname").and_then(|aaname|{
+                Ok(aaname.as_node().text_contents())
+            }).expect("Cannot get anime name");
+    
+            anime.push(Anime {
+                 name: (name), url: (format!("https://jut.su{}", url)), image_url: (image) 
+            });
         });
-    });
+    
+        Ok(anime)
+    }
 
-    Ok(anime)
+    pub async fn get_epidoes(&self) -> Vec<Episode> {
+        let client = create_http_client();
+
+        let resp = client.get(&self.url).send().await
+            .expect("Cannot get anime page content");
+        
+        let response = resp.error_for_status()
+            .expect("Cannot get anime page content");
+
+        let page_content = response.text().await.expect("Cannot get page content");
+        let document = kuchiki::parse_html().one(page_content);
+
+        let mut episodes: Vec<Episode> = Vec::new();
+
+        document.select(".video")
+            .expect("Cannot find .video elements").for_each(|node| {
+                let url = node.attributes.try_borrow().and_then(|attrs|{
+                    Ok(attrs.get("href").expect("Cannot find href attribute").to_string())
+                }).unwrap();
+                
+                episodes.push(Episode {
+                    url: (format!("http://jut.su{}", url)) 
+                });
+            });
+
+        episodes
+    }
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,13 +1,22 @@
-
-
 #[cfg(test)]
-mod anime_list_tests {
+mod tests {
     #[tokio::test]
     async fn get_anime_test() {
-        let animes = jutsuparser_rs::search("Две звезды")
+        let anime = jutsuparser_rs::Anime::search("Две звезды")
             .await
             .expect("Cannot get anime");
-        let first_anime = animes.iter().next().expect("Cannot get anime");
+        let first_anime = anime.iter().next().expect("Cannot get anime");
         assert_eq!(first_anime.url, "https://jut.su/onmyouji/")
+    }
+
+    #[tokio::test]
+    async fn get_epidoe_test() {
+        let anime = jutsuparser_rs::Anime::search("Две звезды")
+            .await
+            .expect("Cannot get anime");
+        let first_anime = anime.iter().next().expect("Cannot get anime");
+        let episodes = first_anime.get_epidoes().await;
+
+        assert_eq!(episodes.iter().next().unwrap().get_name().await, "Две судьбы: парень встречает девушку")
     }
 }
